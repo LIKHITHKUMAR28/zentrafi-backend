cbc3de7dad3b41684c3d53757d11888543a0f95c
Likhith Kumar <ljaganna@gitam.in>
2025-06-02 20:31:46 +0530

    Fix CORS for Vercel frontend

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const app = express();
 
 app.use(cors({
   origin: 'https://zentrafi-frontend.vercel.app',
+    allowedHeaders: ['Content-Type', 'Authorization'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
 
@@ -21,11 +21,9 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
+
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/transactions', require('./routes/transactionRoutes'));
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () =>
-  console.log(`Server started on port ${PORT}`)
-);
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
